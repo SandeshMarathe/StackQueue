5d7ab6377febd2e6f8a9db6b0c4818fd2e662c25
SandeshMarathe <marathesandesh1999@gmail.com>
2021-12-24 11:32:01 +0530

    ADDED: Ability to Create a Queue

--- StackQueue.java
+++ StackQueue.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 class UserInputs {
     private static Scanner scanner = new Scanner(System.in);
     public static int intVal(){return scanner.nextInt();};
@@ -11,7 +10,7 @@ public class StackQueue {
         StackQueueOperation stackQueueOperation = new StackQueueOperation();
         int choice = 0;
         do {
-            System.out.println("1.PUSH\n2.POP\n3.EnQueue\n4.DeQueue\n5.Print\n6.Exit");
+            System.out.println("1.PUSH\n2.POP\n3.EnQueue\n4.DeQueue\n5.Print-Stack\n6.Print-Queue\n7.Exit");
             System.out.println("Enter Your Choice :");
             choice = UserInputs.intVal();
 
@@ -24,6 +23,8 @@ public class StackQueue {
                     stackQueueOperation.pop();
                     break;
                 case 3:
+                    System.out.println("Enter the Element :");
+                    stackQueueOperation.enQueue(UserInputs.intVal());
                     break;
                 case 4:
                     break;
@@ -31,19 +32,24 @@ public class StackQueue {
                     stackQueueOperation.print();
                     break;
                 case 6:
+                    stackQueueOperation.printQueue();
+                    break;
+                case 7:
                     break;
                 default:
                     System.out.println("Invalid Choice..!");
                     break;
             }
-        } while (choice != 6);
+        } while (choice != 7);
     }
 }
 
 class StackQueueOperation {
 
     int[] myStack = new int[5];
-    int top = -1;
+    int[] myQueue = new int[5];
+    int top = -1, rear = -1, front = -1;
+
     public boolean isFull() {
         if (top == myStack.length-1) {
             return true;
@@ -81,6 +87,35 @@ class StackQueueOperation {
     public void print() {
         for (int i = top; i >= 0; i--) {
             System.out.println(myStack[i]);
+        }
+    }
+
+    public boolean isQueueFull() {
+        if (front == 0 && rear == myQueue.length-1) { //|| front >= rear
+            return true;
+        }
+        return false;
+    }
+
+    public void enQueue(int ele) {
+        if (isQueueFull()) {
+            System.out.println("Stack Queue is Full..!");
+        }
+        else {
+            if (front == -1) {
+                front = 0;
+                rear = 0;
+            }
+            else {
+                rear++;
+            }
+            myQueue[rear] = ele;
+        }
+    }
+
+    public void printQueue() {
+        for (int i = front; i <= rear; i++) {
+            System.out.println(myQueue[i]);
         }
     }
 }
